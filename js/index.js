5ffbd03afb74605c549508fcaf5752ca8a849f23
LiamColeE <Liamedlinger@gmail.com>
2019-06-08 11:06:09 -0600

    fixed an issue with the rotating page that was introduced on the ast commit

--- js/index.js
+++ js/index.js
@@ -5,7 +5,8 @@
 //
 let mouseX = 0;
 let mouseY = 0;
-document.addEventListener("mousemove", function(e){
+
+document.addEventListener("mousemove", function (e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
     drag();
@@ -16,22 +17,21 @@ document.addEventListener("mousemove", function(e){
 // 
 let body = document.querySelector("body");
 let rotation = 0;
-let ticking = false;
 
 document.addEventListener("scroll", function (e) {
     console.log("rotate");
-    if (!ticking) {
-        rotation += 1;
-        if (rotation == 360) {
-            rotation = 0;
-        }
-        body.style.transform = `rotate(${rotation}deg)`;
-        ticking = false;
+    rotation += 1;
+    if (rotation == 360) {
+        rotation = 0;
     }
+    body.style.transform = `rotate(${rotation}deg)`;
 
-    ticking = true;
 })
 
+
+//
+// Make Header img dragable
+// 
 let headerImg = document.querySelector(".container .intro img");
 let clicked = false;
 
@@ -39,15 +39,15 @@ headerImg.addEventListener('mousedown', event => {
     headerImg.style.position = "absolute";
     clicked = true;
     console.log(event);
-  });
+});
 
 
 headerImg.addEventListener('mouseup', event => {
     clicked = false;
 });
 
-function drag(){
-    if(clicked){
+function drag() {
+    if (clicked) {
         headerImg.style.left = `${mouseX}px`
         headerImg.style.top = `${mouseY}px`
     }
