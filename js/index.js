181979341e7c4382d49409f2b76ee0631ec65e57
LiamColeE <Liamedlinger@gmail.com>
2019-06-08 10:26:26 -0600

    first event created

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// 
+// Rotate Page OnScroll
+// 
+let body = document.querySelector("body");
+let rotation = 0;
+
+document.addEventListener("scroll", function(e) {
+    console.log("rotate");
+    rotation += 1;
+    if(rotation == 360){
+        rotation = 0;
+    }
+    body.style.transform = `rotate(${rotation}deg)`;
+})
+
